79d8fd22c98fe9b00e5c76dc5cbc03afb410977a
Amira Ashmawy <a.o.amira17@gmail.com>
2015-08-05 00:28:19 +0200

    modefied main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,16 @@
 #include <QApplication>
 #include <QGraphicsScene>
-#include "MyRect.h"
+#include "demond.h"
 #include <QGraphicsView>
+#include "bodygame.h"
+#include<QDebug>
 
-void creatingDiamonds(QGraphicsScene *sc){
-    MyRect *demond1 =new MyRect();
-    demond1 ->setRect(55,-300,100,50);
-
-    sc ->addItem(demond1);
-
-    MyRect *demond2 =new MyRect();
-    demond2 ->setRect(-55,-300,100,50);
-
-    sc ->addItem(demond2);
-
-    MyRect *demond3 =new MyRect();
-    demond3 ->setRect(165,-300,100,50);
-
-    sc ->addItem(demond3);
-
-    MyRect *demond4 =new MyRect();
-    demond4 ->setRect(-165,-300,100,50);
-
-    sc ->addItem(demond4);
-}
-
+BodyGame * game ;
 int main(int argc, char *argv[])
 {
+    game = new BodyGame();
     QApplication a(argc, argv);
-
-    QGraphicsScene *sc = new QGraphicsScene();
-
-    MyRect *rec =new MyRect();
-    rec ->setRect(0,300,100,10);
-
-    sc ->addItem(rec);
-
-    creatingDiamonds(sc);
-
-    rec -> setFlag(QGraphicsRectItem::ItemIsFocusable);
-    rec -> setFocus();
-
-    QGraphicsView * vi = new QGraphicsView(sc);
-    vi -> show();
-
+    game->show();
     return a.exec();
 }
 
