e1f1f418c63706068cccaf5b0d7ea8bf089ed024
hend <hend.com13@gmail.com>
2015-08-05 16:05:17 +0200

    edit

--- main.cpp
+++ main.cpp
@@ -2,46 +2,54 @@
 #include <QGraphicsScene>
 #include "MyRect.h"
 #include <QGraphicsView>
+#include <vector>
+using namespace std ;
 
 void creatingDiamonds(QGraphicsScene *sc){
-    MyRect *demond1 =new MyRect();
-    demond1 ->setRect(55,-300,100,50);
+    vector <MyRect *>demond(10);
+    //MyRect *= new MyRect();
+    for (int i = 0 , j= 100 ; i < 9; j+= 100,++i) {
+        demond[i]=new MyRect();
+        demond[i] ->setRect(55+j,-300,100,50);
+        sc ->addItem(demond[i]);
+    }
 
-    sc ->addItem(demond1);
 
-    MyRect *demond2 =new MyRect();
-    demond2 ->setRect(-55,-300,100,50);
 
-    sc ->addItem(demond2);
 
-    MyRect *demond3 =new MyRect();
-    demond3 ->setRect(165,-300,100,50);
+//    MyRect *demond2 =new MyRect();
+//    demond2 ->setRect(-55,-300,100,50);
 
-    sc ->addItem(demond3);
+//    sc ->addItem(demond2);
 
-    MyRect *demond4 =new MyRect();
-    demond4 ->setRect(-165,-300,100,50);
+//    MyRect *demond3 =new MyRect();
+//    demond3 ->setRect(165,-300,100,50);
 
-    sc ->addItem(demond4);
+//    sc ->addItem(demond3);
+
+//    MyRect *demond4 =new MyRect();
+//    demond4 ->setRect(-165,-300,100,50);
+
+//    sc ->addItem(demond4);
 }
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QGraphicsScene *sc = new QGraphicsScene();
+    QGraphicsScene *scene = new QGraphicsScene();
 
     MyRect *rec =new MyRect();
     rec ->setRect(0,300,100,10);
 
-    sc ->addItem(rec);
+    scene ->addItem(rec);
 
-    creatingDiamonds(sc);
+    creatingDiamonds(scene);
 
     rec -> setFlag(QGraphicsRectItem::ItemIsFocusable);
     rec -> setFocus();
 
-    QGraphicsView * vi = new QGraphicsView(sc);
+    QGraphicsView * vi = new QGraphicsView(scene);
     vi -> show();
 
     return a.exec();
